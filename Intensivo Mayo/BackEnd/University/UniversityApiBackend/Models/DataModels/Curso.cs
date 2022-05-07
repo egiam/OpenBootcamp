150d473977b6e7c210dd2f1f5b9c30681db60e5f
egiam <ezegiampaoli@gmail.com>
2022-05-07 15:51:16 -0300

    Update Curso.cs

--- Intensivo Mayo/BackEnd/University/UniversityApiBackend/Models/DataModels/Curso.cs
+++ Intensivo Mayo/BackEnd/University/UniversityApiBackend/Models/DataModels/Curso.cs
@@ -1,6 +1,17 @@
-﻿namespace UniversityApiBackend.Models.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
 {
     public class Curso
     {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(280)]
+        public string ShrotDescription { get; set; } = string.Empty;
+        public string LongDescription { get; set; } = string.Empty;
+        public string PublicObjective { get; set; } = string.Empty;
+        public string Objectives { get; set; } = string.Empty;
+        public string Requirments { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
     }
 }
